42a9410648ecad58ba138f1569ce62edf7540a38
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-28 12:04:43 +0200

    Update PartsGridData.cs

--- Assets/Game/Scripts/Vehicle/Building/PartsGridData.cs
+++ Assets/Game/Scripts/Vehicle/Building/PartsGridData.cs
@@ -5,5 +5,10 @@ namespace Vehicle.Building
     public class PartsGridData
     {
         public PartConfig[,] partConfigs;
+
+        public PartsGridData()
+        {
+            partConfigs = new PartConfig[0, 0];
+        }
     }
 }
