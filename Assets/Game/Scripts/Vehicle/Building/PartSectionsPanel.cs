62e0b909370c50f9fd9c807fbd252d53c7261d98
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-28 14:17:36 +0200

    Fixed parts section panel

--- Assets/Game/Scripts/Vehicle/Building/PartSectionsPanel.cs
+++ Assets/Game/Scripts/Vehicle/Building/PartSectionsPanel.cs
@@ -14,12 +14,15 @@ namespace Vehicle.Building
         {
             _partSections = new PartSection[partsGridConfig.Size.rows, partsGridConfig.Size.columns];
 
+            float offsetX = (partsGridConfig.Size.columns - 1) / 2f;
+            float offsetY = (partsGridConfig.Size.rows - 1) / 2f;
+
             for (int rowIndex = 0; rowIndex < _partSections.GetLength(0); rowIndex++)
             {
                 for (int columnIndex = 0; columnIndex < _partSections.GetLength(1); columnIndex++)
                 {
-                    PartSection partSection = Instantiate(_partSectionPrefab);
-                    partSection.transform.position = new Vector3(columnIndex, rowIndex, 0f);
+                    PartSection partSection = Instantiate(_partSectionPrefab, transform);
+                    partSection.transform.localPosition = new Vector3(columnIndex - offsetX, rowIndex - offsetY, 0f);
 
                     _partSections[rowIndex, columnIndex] = partSection;
                 }
