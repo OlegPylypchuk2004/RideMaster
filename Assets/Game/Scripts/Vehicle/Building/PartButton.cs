d1fd896da159267436e681624124a1193db3dfe5
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-30 22:02:02 +0200

    Changed part button

--- Assets/Game/Scripts/Vehicle/Building/PartButton.cs
+++ Assets/Game/Scripts/Vehicle/Building/PartButton.cs
@@ -9,6 +9,8 @@ namespace Vehicle.Building
     public class PartButton : MonoBehaviour, IPointerDownHandler
     {
         [SerializeField] private PartConfig _partConfig;
+        [SerializeField] private GameObject _enabledDisplay;
+        [SerializeField] private GameObject _disbledDisplay;
         [SerializeField] private Image _iconImage;
 
         public event Action<PartConfig> Selected;
@@ -17,11 +19,16 @@ namespace Vehicle.Building
         {
             if (_partConfig == null)
             {
+                _enabledDisplay.SetActive(false);
+                _disbledDisplay.SetActive(true);
+
                 return;
             }
 
+            _enabledDisplay.SetActive(true);
+            _disbledDisplay.SetActive(false);
+
             _iconImage.sprite = _partConfig.IconSprite;
-            _iconImage.SetNativeSize();
         }
 
         public void OnPointerDown(PointerEventData eventData)
