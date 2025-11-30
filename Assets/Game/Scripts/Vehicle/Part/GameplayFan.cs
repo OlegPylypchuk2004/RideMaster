d9ac04c5f28880747203cc120bbfde28bfb7ae9a
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-11-30 13:43:11 +0200

    Added gameplay fan part

---
+++ Assets/Game/Scripts/Vehicle/Part/GameplayFan.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Vehicle.Part
+{
+    public class GameplayFan : GameplayPart
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private float _force;
+
+        private bool _isAccelerate;
+
+        private void Update()
+        {
+            _isAccelerate = Input.GetMouseButton(0);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_isAccelerate)
+            {
+                _rigidbody.AddForce(transform.forward * _force, ForceMode.Acceleration);
+            }
+        }
+    }
+}
