23c25d5214719e239de97b2e067a283cafe84e88
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-12-02 12:21:42 +0200

    Added location button script

---
+++ Assets/Game/Scripts/Menu/LocationButton.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    public class LocationButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        public event Action Clicked;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            Clicked?.Invoke();
+        }
+    }
+}
