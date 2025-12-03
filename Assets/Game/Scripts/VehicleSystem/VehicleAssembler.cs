a980d83c615b09d2ecd1a5092864e480fc4763e3
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-12-03 22:28:05 +0200

    Update VehicleAssembler.cs

--- Assets/Game/Scripts/VehicleSystem/VehicleAssembler.cs
+++ Assets/Game/Scripts/VehicleSystem/VehicleAssembler.cs
@@ -51,60 +51,6 @@ namespace VehicleSystem
                     _vehicle.AddPart(part);
                 }
             }
-
-            //CreateJoints(spawnedParts, rows, columns);
-        }
-
-        private void CreateJoints(GameplayPart[,] parts, int rows, int cols)
-        {
-            Vector2Int[] directions =
-            {
-                new Vector2Int(1, 0),
-                new Vector2Int(0, 1)
-            };
-
-            for (int x = 0; x < rows; x++)
-            {
-                for (int y = 0; y < cols; y++)
-                {
-                    GameplayPart currentPart = parts[x, y];
-
-                    if (currentPart == null)
-                    {
-                        continue;
-                    }
-
-                    foreach (var dir in directions)
-                    {
-                        GameplayPart neighbor = GetNeighbor(parts, x, y, dir, rows, cols);
-
-                        if (neighbor != null)
-                        {
-                            ConnectParts(currentPart, neighbor);
-                        }
-                    }
-                }
-            }
-        }
-
-        private void ConnectParts(GameplayPart part1, GameplayPart part2)
-        {
-            FixedJoint joint = part1.gameObject.AddComponent<FixedJoint>();
-            joint.connectedBody = part2.GetComponent<Rigidbody>();
-            joint.enableCollision = false;
-        }
-
-        private GameplayPart GetNeighbor(GameplayPart[,] parts, int x, int y, Vector2Int direction, int rows, int columns)
-        {
-            int newX = x + direction.x;
-            int newY = y + direction.y;
-
-            if (newX >= 0 && newX < rows && newY >= 0 && newY < columns)
-            {
-                return parts[newX, newY];
-            }
-
-            return null;
         }
     }
 }
