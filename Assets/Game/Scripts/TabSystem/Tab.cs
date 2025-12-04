bf8482f1378f1e52488e7c035df1f8f2cdffa294
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-12-04 19:28:36 +0200

    Changed tabs

--- Assets/Game/Scripts/TabSystem/Tab.cs
+++ Assets/Game/Scripts/TabSystem/Tab.cs
@@ -4,14 +4,22 @@ namespace TabSystem
 {
     public class Tab : MonoBehaviour
     {
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private CanvasGroup _canvasGroup;
+
         public virtual void Activate()
         {
-            gameObject.SetActive(true);
+            _canvasGroup.gameObject.SetActive(true);
         }
 
         public virtual void Deactivate()
         {
-            gameObject.SetActive(false);
+            _canvasGroup.gameObject.SetActive(false);
+        }
+
+        public void ApplySize(float width, float height)
+        {
+            _rectTransform.sizeDelta = new Vector2(width, height);
         }
     }
 }
--- Assets/Game/Scripts/TabSystem/TabChanger.cs
+++ Assets/Game/Scripts/TabSystem/TabChanger.cs
@@ -1,18 +1,44 @@
+using DG.Tweening;
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace TabSystem
 {
     public class TabChanger : MonoBehaviour
     {
+        [SerializeField, Min(0)] private int _initialTabIndex;
+        [SerializeField] private Tab[] _tabs;
         [SerializeField] private TabButton[] _tabButtons;
-        [SerializeField] private Tab _initialTab;
+        [SerializeField] private RectTransform _tabsParentRectTransform;
+        [SerializeField] private CanvasGroup _tabsParentCanvasGroup;
+        [SerializeField, Min(0f)] private float _duration;
+        [SerializeField] private Ease _ease;
 
-        private Tab _activeTab;
+        private int _activeTabIndex;
+        private Sequence _currentSequence;
 
         private void Awake()
         {
-            _activeTab = _initialTab;
-            _activeTab.Activate();
+            Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+
+            _initialTabIndex = Mathf.Clamp(_initialTabIndex, 0, _tabs.Length - 1);
+            _activeTabIndex = _initialTabIndex;
+            _tabsParentRectTransform.anchoredPosition = new Vector3(screenSize.x * _activeTabIndex * -1f, 0f, 0f);
+
+            for (int i = 0; i < _tabs.Length; i++)
+            {
+                _tabs[i].ApplySize(screenSize.x, screenSize.y);
+
+                if (i == _activeTabIndex)
+                {
+                    _tabs[i].Activate();
+                }
+                else
+                {
+                    _tabs[i].Deactivate();
+                }
+            }
         }
 
         private void OnEnable()
@@ -33,23 +59,43 @@ namespace TabSystem
 
         private void OnTabButtonSelected(Tab tab)
         {
-            if (tab == null)
+            if (tab == null || !_tabs.Contains(tab) || _tabs[_activeTabIndex] == tab)
             {
                 return;
             }
 
-            if (_activeTab != null)
+            _activeTabIndex = Array.IndexOf(_tabs, tab);
+            _tabsParentCanvasGroup.interactable = false;
+
+            _currentSequence?.Kill();
+            _currentSequence = DOTween.Sequence();
+            _currentSequence.SetLink(gameObject);
+
+            _currentSequence.AppendCallback(() =>
             {
-                if (_activeTab == tab)
+                for (int i = 0; i < _tabs.Length; i++)
                 {
-                    return;
+                    _tabs[i].Activate();
+                }
+            });
+
+            _currentSequence.Append(_tabsParentRectTransform.DOMoveX(Screen.width * _activeTabIndex * -1f, _duration)
+                .SetEase(_ease));
+
+            _currentSequence.AppendCallback(() =>
+            {
+                for (int i = 0; i < _tabs.Length; i++)
+                {
+                    if (i == _activeTabIndex)
+                    {
+                        continue;
+                    }
+
+                    _tabs[i].Deactivate();
                 }
 
-                _activeTab.Deactivate();
-            }
-
-            _activeTab = tab;
-            _activeTab.Activate();
+                _tabsParentCanvasGroup.interactable = true;
+            });
         }
     }
 }
