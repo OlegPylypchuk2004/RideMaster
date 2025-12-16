b584284a8e94bb480746275055b00ce5a87d7749
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-12-16 10:44:50 +0200

    Added level complete continue button

--- Assets/Game/Scripts/UI/PopupSystem/Popups/LevelCompletedPopup.cs
+++ Assets/Game/Scripts/UI/PopupSystem/Popups/LevelCompletedPopup.cs
@@ -1,9 +1,39 @@
+using SceneLoadingSystem;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace UI.PopupSystem.Popups
 {
     public class LevelCompletedPopup : Popup
     {
+        [SerializeField] private Button _continueButton;
 
+        private SceneLoader _sceneLoader;
+
+        [Inject]
+        private void Construct(SceneLoader sceneLoader)
+        {
+            _sceneLoader = sceneLoader;
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _continueButton.onClick.AddListener(OnContinueButtonClicked);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            _continueButton.onClick.RemoveListener(OnContinueButtonClicked);
+        }
+
+        private void OnContinueButtonClicked()
+        {
+            _sceneLoader.Load(0);
+        }
     }
 }
