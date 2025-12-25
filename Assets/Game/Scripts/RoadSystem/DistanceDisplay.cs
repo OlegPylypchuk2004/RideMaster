dddc4ed5866a36972bd73bda661bff841c0c965e
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2025-12-25 17:35:13 +0200

    Changed distance display

--- Assets/Game/Scripts/RoadSystem/DistanceDisplay.cs
+++ Assets/Game/Scripts/RoadSystem/DistanceDisplay.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using VehicleSystem;
 using Zenject;
 
 namespace RoadSystem
@@ -9,16 +10,18 @@ namespace RoadSystem
         [SerializeField] private Image _progressImage;
 
         private Road _road;
+        private Vehicle _vehicle;
 
         [Inject]
-        private void Construct(Road road)
+        private void Construct(Road road, Vehicle vehicle)
         {
             _road = road;
+            _vehicle = vehicle;
         }
 
         private void LateUpdate()
         {
-            _progressImage.fillAmount = _road.VehicleSplineTime;
+            _progressImage.fillAmount = Mathf.InverseLerp(_road.VehicleStartPoint.z, _road.FinishFlag.transform.position.z, _vehicle.transform.position.z);
         }
     }
 }
--- Assets/Game/Scripts/RoadSystem/Road.cs
+++ Assets/Game/Scripts/RoadSystem/Road.cs
@@ -1,8 +1,5 @@
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
-using VehicleSystem;
-using Zenject;
 
 namespace RoadSystem
 {
@@ -12,55 +9,8 @@ namespace RoadSystem
         [SerializeField] private Vector3 _vehicleStartPoint;
         [SerializeField] private FinishFlag _finishFlag;
 
-        private Vehicle _vehicle;
-        private Vector3 _vehicleSplinePosition;
-        private float _vehicleSplineTime;
-
-        public Vector3 VehicleSplinePosition => _vehicleSplinePosition;
-        public float VehicleSplineTime => _vehicleSplineTime;
         public Vector3 VehicleStartPoint => _vehicleStartPoint;
         public FinishFlag FinishFlag => _finishFlag;
-
-        [Inject]
-        private void Construct(Vehicle vehicle)
-        {
-            _vehicle = vehicle;
-        }
-
-        private void Update()
-        {
-            if (_vehicle == null || _splineContainers == null || _splineContainers.Length == 0)
-            {
-                return;
-            }
-
-            float minDistance = float.MaxValue;
-            float3 bestPoint = float3.zero;
-            float bestTime = 0f;
-
-            Vector3 vehiclePosition = _vehicle.transform.position;
-
-            foreach (SplineContainer container in _splineContainers)
-            {
-                if (container == null)
-                {
-                    continue;
-                }
-
-                SplineUtility.GetNearestPoint(container.Spline, vehiclePosition, out float3 point, out float time);
-                float distance = math.distancesq(vehiclePosition, point);
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    bestPoint = point;
-                    bestTime = time;
-                }
-            }
-
-            _vehicleSplinePosition = bestPoint;
-            _vehicleSplineTime = bestTime;
-        }
 
         private void OnDrawGizmos()
         {
